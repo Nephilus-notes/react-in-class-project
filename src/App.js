da7bfa4e70083eed2cd8bff0bc8434e64c4b3f4e
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2022-12-22 13:09:43 -0600

    afternoon push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-// import { useState } from 'react';
+import { useContext } from 'react';
 import Counter from './components/Counter'
 import Students from './components/Students'
 import About from './components/About'
@@ -8,13 +8,15 @@ import Contact from './components/Contact'
 import Pokemon from './components/Pokemon';
 import Blog from './views/Blog';
 import BlogSingle from './views/BlogSingle'
+import { AuthContext } from './contexts/AuthProvider';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
- 
+    const { login, user, logout } = useContext(AuthContext)
 
   return (
     <div className="App">
+      <h2>Loggedin User : { user.username }</h2>
       <BrowserRouter>
       <nav>
         <ul>
@@ -24,6 +26,11 @@ function App() {
           <li><Link to='/contact'>Contact</Link></li>
           <li><Link to='/pokemon'>pokemon</Link></li>
           <li><Link to='/blog'>Blog</Link></li>
+          {
+            (user.loggedIn) ?
+          <li><button onClick={ logout }>Log Out</button></li> :
+          <li><button onClick={ login }>Login</button></li>
+          }
         </ul>
       </nav>
       <Routes>
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,7 +1,53 @@
-import { useContext, creatContext, useEffect, useState } from "react";
+import { useContext, createContext, useEffect, useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "@firebase/auth"
+
 
 export const AuthContext = createContext()
 
 export const AuthProvider = function(props) {
-    
+const [user, setUser] = useState({
+    loggedIn: false
+})
+
+    const auth = getAuth()
+    const provider = new GoogleAuthProvider()
+
+    async function login() {
+        const result = await signInWithPopup(auth, provider)  
+        console.log(result)
+    }
+
+    async function logout() {
+        const result = await signOut(auth)
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (userInfo) => {
+            console.log(userInfo)
+        if (userInfo) {
+            setUser({
+                email: userInfo.email,
+                username: userInfo.displayName,
+                uid: userInfo.uid,
+                loggedIn: true
+            })
+        } else {
+            setUser({
+                loggedIn: false
+            })
+        }
+        console.log(userInfo)})
+    }, [])
+
+    const value = {
+        login,
+        user,
+        logout
+    }
+
+    return(
+        <AuthContext.Provider value={value}>
+            {props.children}
+        </AuthContext.Provider>
+    )
 }
