f17900e87bb85126969ee864520492fe7a3a66a9
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2022-12-22 15:44:52 -0600

    fixed index import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { DataProvider } from './contexts/dataProvider';
+import { DataProvider } from './contexts/DataProvider';
 import { AuthProvider } from './contexts/AuthProvider';
 import reportWebVitals from './reportWebVitals';
 import firebase from './firebase'
